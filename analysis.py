1a99a621fdc0e2ce47543e8de4dbc4d3ec4c4773
612yese <240590367@qq.com>
2020-11-27 20:36:17 +0800

    tmp

--- analysis.py
+++ analysis.py
@@ -408,6 +408,14 @@ def handle_chusan():
 
 
 if __name__ == '__main__':
+    print('\n'.join([''.join([('Love'[(x - y) % len('Love')]
+                               if ((x * 0.05) ** 2 + (y * 0.1) ** 2 - 1) ** 3 - (x * 0.05) ** 2 * (
+            y * 0.1) ** 3 <= 0 else ' ')  # 此处是根据心形曲线公式来的(x2+y2-1)3-x2y3=0
+                              for x in range(-30, 30)])  # 定义高
+                     for y in range(30, -30, -1)]))  # 定义宽
+    print("这是给鸣夏小朋友的礼物")
+    print()
+
     if os.path.exists("初一成绩"):
         handle_chuyi()
 
